b1d88ea1aae07bcdcd298f09986a77982bc2809f
Aleksandr Feodorov <webmaster@unixcomp.org>
2020-08-01 23:17:43 +0300

    Func ready

--- src/e97.s
+++ src/e97.s
@@ -6,6 +6,11 @@ mask: .long 0x00000001, 0x00000001, 0x00000001, 0x00000001
 
   .text
 
+# uint8_t func(uint8_t *a, uint8_t *b, uint8_t *c, uint8_t *d)
+# {
+#    prepare_args(a, b, c, d);
+#    return (*a | *b) & (!*a | *c) & (!*b | !*c) & (!*a | !*c | d);
+# }
 func:
   enter $0, $0
   sub $8+16, %esp # stack align
@@ -13,6 +18,49 @@ func:
   movdqa %xmm0, (%esp)
   call prepare_args
   add $8+16, %esp
+
+  # (1) = *a | *b
+  mov 8(%ebp), %eax # &a
+  mov (%eax), %al # *a
+  mov 8+4*1(%ebp), %ebx # &b
+  mov (%ebx), %bl # *b
+  mov %bl, %bh
+  or %al, %bh
+  mov %bh, %dl
+  shl $8, %dx
+
+  # (2) = !*a | *c
+  mov 8+4*2(%ebp), %ecx # &c
+  mov (%ecx), %cl # *c
+  not %al
+  mov %cl, %ch
+  or %al, %ch
+  mov %ch, %dl
+  shl $8, %edx
+
+  # (3) = !*b | !*c
+  not %cl
+  not %bl
+  or %cl, %bl
+  mov %bl, %dl
+
+  # (4) = !*a | !*c | *d
+  mov 8+4*3(%ebp), %ebx # &d
+  mov (%ebx), %bl # *d
+  or %cl, %al
+  or %bl, %al
+
+  # (5) = (3) & (4)
+  and %dl, %al
+
+  # (6) = (2) & (5)
+  shr $8, %edx
+  and %dl, %al
+
+  # (7) = (1) & (6)
+  shr $8, %dx
+  and %dl, %al
+
   leave
   ret
 
